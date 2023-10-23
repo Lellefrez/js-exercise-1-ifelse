c60b5d2e93037c8c49ff82d6e7fdb581cbdf931e
Manuel Frezzato <manuel.frezzato@gmail.com>
2023-10-23 17:14:05 +0200

    feat | Convertitora temperatura

--- main.js
+++ main.js
@@ -14,3 +14,15 @@ let AnnoDiNascita = prompt("Indica il tua anno di nascita");
 let Annoattuale = 2023;
 let calcolo = Annoattuale - AnnoDiNascita;
 alert(calcolo); */
+
+/* Convertitore temperatura */
+let temperatura = prompt("Indica una temperatura in fahrenheit");
+let Celsius1 = 32;
+let Celsius2 = 9;
+let Celsius3 = 5;
+let calcolo1 = temperatura - Celsius1;
+let calcolo2 = calcolo1 / Celsius2;
+let calcolo3 = calcolo2 * Celsius3;
+
+let frase = " ° Celsius";
+alert(calcolo3 + frase);
