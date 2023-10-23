ceb1ec32add5cb999644311c91c2e509b5963206
Manuel Frezzato <manuel.frezzato@gmail.com>
2023-10-23 17:01:40 +0200

    feat | calcolatore età

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
+/* Il Caffè */
+/*
 let tazzeVendute = prompt("quante tazze hai venduto oggi?");
 let prezzo = 2;
 let guadagni = tazzeVendute * prezzo;
 let messaggio = "Oggi hai venduto ";
 let fine = " tazze di caffè";
 let frase = messaggio + guadagni + fine;
-alert(frase);
+alert(frase); */
+
+/* Calcolatore età */
+/*
+let AnnoDiNascita = prompt("Indica il tua anno di nascita");
+let Annoattuale = 2023;
+let calcolo = Annoattuale - AnnoDiNascita;
+alert(calcolo); */
