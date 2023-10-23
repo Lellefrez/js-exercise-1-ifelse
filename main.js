f7ba83d457bb5023317dc4d079229e533bcf6ef8
Manuel Frezzato <manuel.frezzato@gmail.com>
2023-10-23 17:36:00 +0200

    feat | Media voti

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ let calcolo = Annoattuale - AnnoDiNascita;
 alert(calcolo); */
 
 /* Convertitore temperatura */
+/*
 let temperatura = prompt("Indica una temperatura in fahrenheit");
 let Celsius1 = 32;
 let Celsius2 = 9;
@@ -26,3 +27,15 @@ let calcolo3 = calcolo2 * Celsius3;
 
 let frase = " ° Celsius";
 alert(calcolo3 + frase);
+*/
+
+/* Calcolatore di voti */
+
+let studente1 = parseFloat(prompt("Indica la valutazione del tuo test"));
+let studente2 = parseFloat(prompt("Indica la valutazione del tuo test"));
+let studente3 = parseFloat(prompt("Indica la valutazione del tuo test"));
+let divisore = 3;
+let calcolo1 = studente1 + studente2 + studente3;
+let calcolo2 = calcolo1 / divisore;
+let frase = "La media delle vostre valutazioni è ";
+alert(frase + calcolo2);
